84707dd4094121fb9db3ac2fd67f0ea5de54a9cc
Maisnerr <maisner.podolsky@gmail.com>
2025-12-19 10:01:34 +0100

    pokracovani

---
+++ apps/server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = "Content-Type"
+
+@app.route("/", methods=["GET"])
+def root():
+    return jsonify({"organisation": "Student Cyber Games"}), 200
+
+@app.route("/courses", methods=["GET"])
+def get_courses():
+    pass
+    ## Do after databases
+
+## ...
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=3000)
