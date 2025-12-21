9b85b7437c18b6d1731c1214cb52c18b872e70d8
Maisnerr <maisner.podolsky@gmail.com>
2025-12-21 13:03:54 +0100

    pravdepodobne funkcni

---
+++ apps/web/app_front.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="frontend", static_url_path="")
+
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.route("/login")
+def test():
+    return send_from_directory(app.static_folder, "login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3001)
